225fcb8d925a7a1aaf6c855330a9bfd9c421447b
Jordan Kisa <jordan.kisa@softwire.com>
2021-07-01 15:11:46 +0100

    Refactored code into functions

--- index.ts
+++ index.ts
@@ -1,42 +1,67 @@
-
-const readline = require('readline-sync');
-console.log('\nEnter the bus stop code');
-const stopcode = readline.prompt();
-// 490008660N
-
 import {BusArrival} from "./busArrival";
-const axios = require('axios').default;
+import axios from "axios";
+const readline = require('readline-sync');
+
+/**
+ * Ask user for STOP ID and output next 5 buses
+ */
+function main(){
+    console.log('\nEnter the bus stop code');
+    const stopcode = readline.prompt();
+    // 490008660N
+
+    let arrivalList = get_stop_arrivals(stopcode)
+    // JSON Object does NOT sort arrivals by time
+}
+
+main()
 
 /**
  * Outputs a list of arrivals of a TFL stop. [BusArrival] Objects store bus route name
  * destination and arrival time
  * @param stopID ID of TFL Bus Stop
  */
-function get_stop_arrivals(stopID:String){
+function get_stop_arrivals(stopID:string) {
+    let arrivalList : BusArrival[] = []
     axios.get('https://api.tfl.gov.uk/StopPoint/' + stopID +'/Arrivals')
-        .then(function (response:any) {
-            let arrivalList : BusArrival[] = []
-
+        .then(function (response) {
             for(let i = 0; i < response.data.length ; i++){
                 arrivalList.push(new BusArrival(
                     response.data[i].lineName,
                     response.data[i].destinationName,
                     response.data[i].timeToStation))
             }
-
-            // JSON Object does NOT sort arrivals by time
             arrivalList.sort((b1:BusArrival,b2:BusArrival) => b1.timeToStation - b2.timeToStation)
-
-            // Print each arrival in readable format
-            for(let i = 0; i < 5; i++){
-                let arrival : BusArrival = arrivalList[i]
-                console.log(arrival.lineName + " to " +
-                     arrival.destinationName + " arriving in " +
-                 Math.floor(arrival.timeToStation/60) + " minutes")
-            }
+            output_arrivals(arrivalList)
         }).catch(function (error:any) {
         console.log(error);
     })
 }
 
-get_stop_arrivals(stopcode)
+/**
+ * Generates the correct eta string based on time
+ * @param eta
+ */
+function generate_eta(eta: number) {
+    if (eta === 0) {
+        return " is Due"
+    } else if (eta === 1) {
+        return " arriving 1 minute"
+    } else {
+        return " arriving in " + eta + " minutes"
+    }
+}
+
+/**
+ * Print each arrival in readable format
+ */
+function output_arrivals(arrivalList:BusArrival[]){
+    for(let i = 0; i < 5; i++){
+        let arrival : BusArrival = arrivalList[i]
+        let eta : number = Math.floor(arrival.timeToStation/60)
+        let etaString = generate_eta(eta);
+        console.log(arrival.lineName + " to " +
+            arrival.destinationName + etaString)
+    }
+}
+
