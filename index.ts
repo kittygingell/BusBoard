7e1bdd18b449afde8389aa19a1fe1e29a7b75a4d
Jordan Kisa <jordan.kisa@softwire.com>
2021-07-01 12:23:37 +0100

    Created function to access arrival json

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
-function edffsd(){
-    console.log('Hello World')
+const axios = require('axios').default;
+
+
+
+function get_stop_arrivals(stopID:String){
+    axios.get('https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals')
+        .then(function (response:any) {
+            console.log(response.data[0])
+        }).catch(function (error:any) {
+        // handle error
+        console.log(error);
+    })
 }
+
+get_stop_arrivals("Hi")
