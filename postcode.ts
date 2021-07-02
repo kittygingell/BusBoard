b0a618756f9460e6621c1fcc1401aee218813f79
Kitty <kittygingell@gmail.com>
2021-07-02 10:34:28 +0100

    Implement promises

--- postcode.ts
+++ postcode.ts
@@ -5,26 +5,19 @@ import {get_nearest_stops} from "./tfl";
  * Returns the latitude and longitude of a given postcode,
  * @param postcode must be a validated postcode
  */
-function get_postcode_location(postcode: string) {
-    axios.get('https://api.postcodes.io/postcodes?q=' + postcode)
-        .then(function (response) {
-            get_nearest_stops(response.data.result[0].longitude.toFixed(3),response.data.result[0].latitude.toFixed(3))
-        }).catch(function (error: any) {
-        console.log(error);
-    })
+async function get_postcode_location(postcode: string) {
+    const response = await axios.get('https://api.postcodes.io/postcodes?q=' + postcode)
+    await get_nearest_stops(response.data.result[0].longitude.toFixed(3),response.data.result[0].latitude.toFixed(3))
 }
 
 /***
  * validates a postcode, if true calls get_postcode_location
  * @param postcode
  */
-export function validate_postcode(postcode: string) {
-    axios.get('https://api.postcodes.io/postcodes/' + postcode + '/validate')
-        .then(function (response) {
-            if (response.data.result) {
-                get_postcode_location(postcode)
-            }
-        }).catch(function (error: any) {
-        console.log(error);
-    })
+export async function validate_postcode(postcode: string) {
+    const response = await axios.get('https://api.postcodes.io/postcodes/' + postcode + '/validate')
+    if (response.data.result) {
+        await get_postcode_location(postcode).then
+    }
+
 }
--- tfl.ts
+++ tfl.ts
@@ -3,28 +3,24 @@ import {BusArrival} from "./busArrival";
 import axios from "axios";
 import {output_arrivals} from "./index";
 
-export function get_nearest_stops(longitude: number, latitude: number) {
+export async function get_nearest_stops(longitude: number, latitude: number) {
     let stopList: NearestStops[] = []
 
-    axios.get('https://api.tfl.gov.uk/StopPoint/?lat=' + latitude + '&lon=%20' + longitude + '&stopTypes=NaptanPublicBusCoachTram&radius=1000')
-        .then(function (response) {
-            for (let i = 0; i < response.data.stopPoints.length; i++) {
-                const stopPoint = response.data.stopPoints[i];
-                if (stopPoint.lines.length == 0){
-                    continue
-                }
-                stopList.push(new NearestStops(
-                    stopPoint.naptanId,
-                    stopPoint.commonName,
-                    stopPoint.distance))
-            }
-            stopList.sort((n1: NearestStops, n2: NearestStops) => n1.distance - n2.distance)
-            for (let i = 0; i < 2 && i < stopList.length; i++) {
-                get_stop_arrivals(stopList[i]);
-            }
-        }).catch(function (error: any) {
-        console.log(error);
-    })
+    const response = await axios.get('https://api.tfl.gov.uk/StopPoint/?lat=' + latitude + '&lon=%20' + longitude + '&stopTypes=NaptanPublicBusCoachTram&radius=1000')
+    for (let i = 0; i < response.data.stopPoints.length; i++) {
+        const stopPoint = response.data.stopPoints[i];
+        if (stopPoint.lines.length == 0){
+            continue
+        }
+        stopList.push(new NearestStops(
+            stopPoint.naptanId,
+            stopPoint.commonName,
+            stopPoint.distance))
+    }
+    stopList.sort((n1: NearestStops, n2: NearestStops) => n1.distance - n2.distance)
+    for (let i = 0; i < 2 && i < stopList.length; i++) {
+        await get_stop_arrivals(stopList[i]);
+    }
 }
 
 /**
@@ -32,22 +28,19 @@ export function get_nearest_stops(longitude: number, latitude: number) {
  * destination and arrival time
  * @param stopID ID of TFL Bus Stop
  */
-function get_stop_arrivals(stop: NearestStops) {
+async function get_stop_arrivals(stop: NearestStops) {
     let arrivalList: BusArrival[] = []
     let stopID = stop.naptanId
-    axios.get('https://api.tfl.gov.uk/StopPoint/' + stopID + '/Arrivals')
-        .then(function (response) {
-            for (let i = 0; i < response.data.length; i++) {
-                arrivalList.push(new BusArrival(
-                    response.data[i].lineName,
-                    response.data[i].destinationName,
-                    response.data[i].timeToStation))
-            }
-            arrivalList.sort((b1: BusArrival, b2: BusArrival) => b1.timeToStation - b2.timeToStation)
-            console.log("***************************")
-            console.log("Arrival times for: " + stop.commonName)
-            output_arrivals(arrivalList)
-        }).catch(function (error: any) {
-        console.log(error);
-    })
+    const response = await axios.get('https://api.tfl.gov.uk/StopPoint/' + stopID + '/Arrivals')
+    for (let i = 0; i < response.data.length; i++) {
+        arrivalList.push(new BusArrival(
+            response.data[i].lineName,
+            response.data[i].destinationName,
+            response.data[i].timeToStation))
+    }
+    arrivalList.sort((b1: BusArrival, b2: BusArrival) => b1.timeToStation - b2.timeToStation)
+    console.log("***************************")
+    console.log("Arrival times for: " + stop.commonName)
+    output_arrivals(arrivalList)
+
 }
